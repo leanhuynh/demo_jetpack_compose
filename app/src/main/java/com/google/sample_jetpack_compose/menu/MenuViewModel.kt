7df157a9af552b08e51a1060419ba8a7ae76984d
leanhuynh <leanhuynh2002@gmail.com>
2022-12-14 02:16:14 +0700

    update

--- app/src/main/java/com/google/sample_jetpack_compose/menu/MenuViewModel.kt
+++ app/src/main/java/com/google/sample_jetpack_compose/menu/MenuViewModel.kt
@@ -12,27 +12,27 @@ class MenuViewModel : ViewModel() {
     private val _data = MutableLiveData(MenuRepository.getMenuData())
     val data: LiveData<Menu> = _data
 
-    fun incrementMenuItemQuantity(menuItem: MenuItem) {
-        _data.value = _data.value!!.let { menu ->
-            menu.copy(
-                menuItems = menu.menuItems.toMutableList().also { menuItems ->
-                    menuItems[menuItems.indexOf(menuItem)] =
-                        menuItem.copy(quantity = menuItem.quantity + 1)
-                }
-            )
-        }
-    }
-
-    fun decrementMenuItemQuantity(menuItem: MenuItem) {
-        _data.value = _data.value!!.let { menu ->
-            menu.copy(
-                menuItems = menu.menuItems.toMutableList().also { menuItems ->
-                    menuItems[menuItems.indexOf(menuItem)] =
-                        menuItem.copy(quantity = menuItem.quantity - 1)
-                }
-            )
-        }
-    }
+//    fun incrementMenuItemQuantity(menuItem: MenuItem) {
+//        _data.value = _data.value!!.let { menu ->
+//            menu.copy(
+//                menuItems = menu.menuItems.toMutableList().also { menuItems ->
+//                    menuItems[menuItems.indexOf(menuItem)] =
+//                        menuItem.copy(quantity = menuItem.quantity + 1)
+//                }
+//            )
+//        }
+//    }
+//
+//    fun decrementMenuItemQuantity(menuItem: MenuItem) {
+//        _data.value = _data.value!!.let { menu ->
+//            menu.copy(
+//                menuItems = menu.menuItems.toMutableList().also { menuItems ->
+//                    menuItems[menuItems.indexOf(menuItem)] =
+//                        menuItem.copy(quantity = menuItem.quantity - 1)
+//                }
+//            )
+//        }
+//    }
 
     // TODO : Is it possible to have recomposition without copying and making a new instance of list item ?
 
